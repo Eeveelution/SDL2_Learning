02a8576c38f823aafa7be9586b6c214776a1803a
= <developerbrzoza@gmail.com>
2021-04-28 15:32:03 +0200

    polling events now aswell

--- main.c
+++ main.c
@@ -14,11 +14,21 @@ int main() {
         SDL_Surface *image = SdlController_LoadImage("test.bmp");
         
         if(image != NULL){
-            SDL_BlitSurface(image, NULL, ctx.screenSurface, NULL);
-    
-            SDL_UpdateWindowSurface(ctx.window);
-    
-            SDL_Delay(15000);
+            bool run = true;
+            //Stores Currently Looping Event
+            SDL_Event event;
+            
+            while(run){
+                //Poll Events
+                while(SDL_PollEvent(&event) != 0) {
+                    if (event.type == SDL_QUIT) {
+                        run = false;
+                    }
+                }
+                
+                SDL_BlitSurface(image, NULL, ctx.screenSurface, NULL);
+                SDL_UpdateWindowSurface(ctx.window);
+            }
         }
     }
 
