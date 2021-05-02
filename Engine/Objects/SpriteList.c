836e4df7525c2a85e19cc3843be903ff090af308
= <developerbrzoza@gmail.com>
2021-05-02 23:10:31 +0200

    struct and functions

---
+++ Engine/Objects/SpriteList.c
@@ -0,0 +1,22 @@
+//
+// Created by eevee on 02.05.21.
+//
+
+#include "SpriteList.h"
+
+void egSpriteList_Init(egSpriteList_t *list) {
+
+}
+
+int egSpriteList_Insert(egSpriteList_t *list, struct Sprite item) {
+    return 0;
+}
+
+void egSpriteList_Remove(egSpriteList_t *list, int index) {
+
+}
+
+void egSpriteList_Free(egSpriteList_t *list) {
+
+}
+
---
+++ Engine/Objects/SpriteList.h
@@ -0,0 +1,20 @@
+//
+// Created by eevee on 02.05.21.
+//
+
+#ifndef SDL2_LEARNING_SPRITELIST_H
+#define SDL2_LEARNING_SPRITELIST_H
+
+#include "Sprite.h"
+
+typedef struct SpriteList {
+    struct Sprite *array;
+    size_t size;
+} egSpriteList_t;
+
+void egSpriteList_Init(egSpriteList_t *list);
+int egSpriteList_Insert(egSpriteList_t *list, struct Sprite item);
+void egSpriteList_Remove(egSpriteList_t *list, int index);
+void egSpriteList_Free(egSpriteList_t *list);
+
+#endif //SDL2_LEARNING_SPRITELIST_H
